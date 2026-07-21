7fcc2916e2d7afd3e8806bfbb868e2bd3d3557a7
RW01TajiPermata <rwtajiagustus@gmail.com>
2026-07-21 19:03:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ HOME_HERO = {
     "covers": [
         "https://lh3.googleusercontent.com/d/1ermCqVeUvMwUDwjDouKPynadjYKq47ma=w2000",
         "https://lh3.googleusercontent.com/d/1sZtrn6-Lg-lgRfHQUPRq2rkmMY61vjaj=w2000",
-        "https://images.unsplash.com/photo-1601050690597-df0568f70950?auto=format&fit=crop&w=1800&q=85",
+        "https://lh3.googleusercontent.com/d/1nFbXprSbIe1x7ljy3xegm0_94ZZtgTe9=w2000",
         "https://images.unsplash.com/photo-1492684223066-81342ee5ff30?auto=format&fit=crop&w=1800&q=85",
     ],
 }
